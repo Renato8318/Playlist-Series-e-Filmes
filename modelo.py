8b0c65d901d95b112f27bdee03b5a98ff3b1d471
Renato Paiva <paivarenato08@gmail.com>
2023-09-04 11:38:15 -0300

    git commit -m "Representação textual de objetos"

--- modelo.py
+++ modelo.py
@@ -20,24 +20,27 @@ class Programa:
     def nome(self, nome_novo):
         self._nome = nome_novo.title()
 
-    def imprime(self):
-        print(f'{self._nome} -  {self.ano} : {self._likes} Likes')
+
+    def __str__(self):
+        return f'{self._nome} -  {self.ano} - {self._likes} Likes'
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
-    def imprime(self):
-        print(f'{self._nome} -  {self.ano} - {self.duracao} min - {self._likes} Likes')
+    def __str__(self):
+        return f'{self._nome} -  {self.ano} - {self.duracao} min - {self._likes} Likes'
+
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
-    def imprime(self):
-        print(f'{self._nome} -  {self.ano} - {self.temporadas} temporadas - {self._likes} Likes')
+    def __str__(self):
+        return f'{self._nome} -  {self.ano} - {self.temporadas} temporadas - {self._likes} Likes'
+
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 vingadores.dar_like()
@@ -48,4 +51,4 @@ atlanta.dar_like()
 filmes_e_series = {vingadores, atlanta}
 
 for programa in filmes_e_series:
-    programa.imprime()
+   print(programa)
