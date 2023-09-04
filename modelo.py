f1ff0d3225669f88b0da16682b892df82bbe6362
Renato Paiva <paivarenato08@gmail.com>
2023-09-04 14:56:14 -0300

    git commit -m "Modelo de dados Python"

--- modelo.py
+++ modelo.py
@@ -44,10 +44,19 @@ class Serie(Programa):
     def __str__(self):
         return f'{self._nome} -  {self.ano} - {self.temporadas} temporadas - {self._likes} Likes'
 
-class Playlist(list):
+class Playlist:
     def __init__(self, nome, programas):
         self.nome = nome
-        super().__init__(programas)
+        self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
+    @property
+    def listagem(self):
+        return self._programas
+
+    def __len__(self):
+        return len(self._programas)
 
 
 
@@ -70,6 +79,9 @@ filmes_e_series = [vingadores, atlanta, demolidor, tmep]
 playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
 print(f'Tamanho do playlist: {len(playlist_fim_de_semana)}')
+
+print(playlist_fim_de_semana[0])
+
 
 for programa in playlist_fim_de_semana:
    print(programa)
